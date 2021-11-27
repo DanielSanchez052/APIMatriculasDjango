896cc340420c65785890310c2828c6d5c631a897
Daniel Sanchez <sanchezd0528@gmail.com>
2021-11-27 00:08:28 -0500

    nah c

--- APIMatriculas/settings/production.py
+++ APIMatriculas/settings/production.py
@@ -5,7 +5,7 @@ from APIMatriculas.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG',False)
-ALLOWED_HOSTS = ['https://apimatriculas.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['apimatriculas.herokuapp.com','https://apimatriculas.herokuapp.com/', 'localhost', '127.0.0.1']
 
 DATABASES={}
 
