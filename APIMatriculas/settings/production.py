de07e289e4243c574b7a4c5ae297a4f0e99d3f36
Daniel Sanchez <sanchezd0528@gmail.com>
2021-11-27 00:06:05 -0500

    DEBUG

--- APIMatriculas/settings/production.py
+++ APIMatriculas/settings/production.py
@@ -4,8 +4,7 @@ import dj_database_url
 from APIMatriculas.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = os.environ.get('DEBUG',False)
 ALLOWED_HOSTS = ['https://apimatriculas.herokuapp.com/', 'localhost', '127.0.0.1']
 
 DATABASES={}
